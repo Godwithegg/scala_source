048f0d86a6a6194720f83d35c1b3b272a1227899
Godwithegg <675175647@qq.com>
2019-11-23 23:29:55 +0800

    scala map映射

---
+++ myscala/src/main/scala/com/danhuang/chapter12/TestClass.scala
@@ -0,0 +1,10 @@
+package com.danhuang.chapter12
+
+object TestClass {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+class AAA extends Item{
+
+}
---
+++ myscala/src/main/scala/com/danhuang/chapter13/PartialFunDemo01.scala
@@ -0,0 +1,16 @@
+package com.danhuang.chapter13
+
+/*
+给你一个集合val list = List(1, 2, 3, 4, "abc") ，请完成如下要求:
+将集合list中的所有数字+1，并返回一个新的集合
+要求忽略掉 非数字 的元素，即返回的 新的集合 形式为 (2, 3, 4, 5)
+
+ */
+object PartialFunDemo01 {
+  def main(args: Array[String]): Unit = {
+    val list = List(1,2,3,4,"abc")
+    //思路1 filter + map方式解决
+
+  }
+
+}
